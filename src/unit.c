1f19feda96a5a661977fe915930332cc49ad5e5e
ConstantRoux <constant.roux31@gmail.com>
2022-01-15 15:08:50 +0100

    Fix a little bug

--- src/unit.c
+++ src/unit.c
@@ -17,7 +17,8 @@ void affect_unit(Unit_u* unit1, Unit_u unit2, Descriptor_e descriptor_type)
     {
         case TEXT:
             unit1->text_descriptor.id = unit2.text_descriptor.id;
-            unit1->text_descriptor.descriptor_contents = unit2.text_descriptor.descriptor_contents;
+            unit1->text_descriptor.descriptor_contents = malloc(MAX_MEMORY_STRING); // TO DO find size
+            strcpy(unit1->text_descriptor.descriptor_contents, unit2.text_descriptor.descriptor_contents);
             break;
 
         case IMAGE:
