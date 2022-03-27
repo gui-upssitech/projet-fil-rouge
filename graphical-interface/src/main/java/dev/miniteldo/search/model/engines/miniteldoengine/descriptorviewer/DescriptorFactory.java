1fa4c8ef75f2d971221a805ba5ed20f61687a04c
constant.roux31@gmail.com <constant.roux31@gmail.com>
2022-03-27 12:36:34 +0200

    Add text descriptor viewer in Java

---
+++ graphical-interface/src/main/java/dev/miniteldo/search/model/engines/miniteldoengine/descriptorviewer/DescriptorFactory.java
@@ -0,0 +1,8 @@
+package dev.miniteldo.search.model.engines.miniteldoengine.descriptorviewer;
+
+public class DescriptorViewer {
+
+    public static Descriptor getDescriptor() {
+
+    }
+}
---
+++ graphical-interface/src/main/java/dev/miniteldo/search/model/engines/miniteldoengine/descriptorviewer/ImageDescriptor.java
@@ -0,0 +1,2 @@
+package dev.miniteldo.search.model.engines.miniteldoengine.descriptorviewer;public class ImageDescriptor {
+}
